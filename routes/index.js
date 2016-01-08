5379a04f496fcdd96703e83637c422f72a19eeb0
qingfengwuyou <h3658686@126.com>
2016-01-08 11:10:38 +0800

    no message

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/angular14/docs/api', function(req, res, next) {
+  res.redirect('/angular14/docs/api/index.html');
+});
 module.exports = router;
