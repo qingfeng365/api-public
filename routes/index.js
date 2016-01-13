ae71b8c92d9e00066dbc3965c7402a85bf96cb4e
qingfengwuyou <h3658686@126.com>
2016-01-13 11:02:11 +0800

    no message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '常用api' });
 });
 
 // router.get('/angular14/docs/api', function(req, res, next) {
@@ -14,4 +14,9 @@ router.get('/', function(req, res, next) {
 router.get('/angular14/*', function(req, res, next) {
   res.redirect('/angular14/docs/index.html');
 });
+
+router.get('/ui.bootstrap.demo-014/*', function(req, res, next) {
+  res.redirect('/ui.bootstrap.demo-014/index.html');
+});
+
 module.exports = router;
